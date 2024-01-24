8649343f0dd39e0dbad411f2db6f6752f0a7c677
Héctor Franco <hhernandezf1800@alumno.ipn.mx>
2024-01-24 13:49:10 -0600

    corrigiendo iteracion de archivos para preprocesar

--- scripts_PDN_Integridat/paso_1_preprocesar_s6_pandas.py
+++ scripts_PDN_Integridat/paso_1_preprocesar_s6_pandas.py
@@ -1,17 +1,13 @@
 import pandas as pd
-import numpy as np
 import os
 import glob
-import json
 import datetime
 from dateutil.parser import parse
 import gc
 from tqdm import tqdm
 tqdm.pandas()
 from itertools import islice
-#import polars as pl
 from pprint import pprint
-from time import sleep
 
 def parse_date(x):
     try:
@@ -40,76 +36,64 @@ def extract_parties_names(parties):
     else:
         return "", ""
 
-def convert_files_parquet_h5(df, directorio_salida, nombre_archivo, i):
-    try:
-        df.to_parquet(directorio_salida+str(nombre_archivo)+"_parquet_" + str(i) + ".parquet")
-    except Exception as e:
-        df.to_hdf(directorio_salida +str(nombre_archivo) + "_s6_hdf_" + str(i) + ".h5", key = "s6_df")
-
-
 def extraer_S6(df):
     keep_cols = ['_id', "ocid", "id", "parties", "awards"]
     df = df[keep_cols]
-    #pprint(df.shape)
-    #pprint(df.columns)
-    res = df.awards.progress_apply(find_dates_no_processing)
-    #pprint(res)
+    res = df.awards.map(find_dates_no_processing)
     df["contractPeriod_startDate"], df["contractPeriod_endDate"] = zip(*res)
     df = df.drop(columns=["awards"])
-    #pprint(df.columns)
-    #pprint(s6_df.head())
     df = df.explode("parties")
     res_contact = df.parties.map(extract_parties_names)
-    #pprint(res_contact)
     df["parties_name"], df["parties_contactPoint_name"] = zip(*res_contact)
     df = df.drop(columns=["parties"])
     df = df.reset_index(drop = True)
-    #pprint(df)
     return df
+
+def convert_files_parquet_h5(df, directorio_salida, nombre_archivo, i):
+    try:
+        nombre_archivo = nombre_archivo.split(".")[0]
+        df.to_parquet(directorio_salida+str(nombre_archivo)+"_parquet_" + str(i) + ".parquet")
+    except Exception as e:
+        nombre_archivo = nombre_archivo.split(".")[0]
+        df.to_hdf(directorio_salida +str(nombre_archivo) + "_s6_hdf_" + str(i) + ".h5", key = "s6_df")
 
 
 ### Leer por carpeta o directorio y nombre de archivo o json
 ### Se iterqa sobre el directorio
-ruta_bulk_s6_todo = '../bulk-s6'
-ruta_bulk_s6 = os.listdir(ruta_bulk_s6_todo)
-pprint(ruta_bulk_s6)
-#print(ruta_bulk_s6[0])
-salida = "salidas_remake_s6_pandas/"
+ruta_bulk_s6_nombres = '../bulk-s6'
+contenido_ruta_bulk_s6 = os.listdir(ruta_bulk_s6_nombres)
+salida_preprocesamiento_s6 = '../salida_paso1_s6_pandas/'
+pprint(ruta_bulk_s6_nombres)
 
-i = 0
-for i in range(len(ruta_bulk_s6)):
-    print(ruta_bulk_s6[i])
-    #df = pd.read_json('bulk-s6/'+ruta_bulk_s6[i], lines=True)
-    if os.path.isdir('bulk-s6/'+ruta_bulk_s6[i]) == True:
-        #print("es un directorio")
-        #print(ruta_bulk_s6[i]) 
-        nombre_directorio = 'bulk-s6/'+ruta_bulk_s6[i]+'/*.json'
-        #print("cadena")
-        #print(nombre_directorio)
-        lista_directorio = glob.glob(nombre_directorio)
-        #pprint(lista_directorios)
+for i in range(len(contenido_ruta_bulk_s6)):
+    print("Iteracion: ", i)
+    ##solo para listar lo que se itera
+    ##print("Archivo: ", contenido_ruta_bulk_s6[i])
+    if os.path.isfile(ruta_bulk_s6_nombres +'/'+contenido_ruta_bulk_s6[i]) == True:
+        #print("Es un archivo")
+        nombre_archivo = contenido_ruta_bulk_s6[i]
+        #print("Archivo: ", nombre_archivo)
+        ruta_archivo = ruta_bulk_s6_nombres +'/'+contenido_ruta_bulk_s6[i]
+        #rint("Ruta archivo: ", ruta_archivo)
+        df = pd.read_json(ruta_bulk_s6_nombres +'/'+contenido_ruta_bulk_s6[i])
+        pprint(df.shape)
+        df = extraer_S6(df)
+        convert_files_parquet_h5(df, salida_preprocesamiento_s6, nombre_archivo, i)
+    if os.path.isdir(ruta_bulk_s6_nombres +'/'+contenido_ruta_bulk_s6[i]) == True:
+        pass
+        print("Es un directorio")
+        ## listar el contenido del subdirectorio
+        contenido_subdirectorio = os.listdir(ruta_bulk_s6_nombres +'/'+contenido_ruta_bulk_s6[i])
         x=0
-        for j in range(len(lista_directorio)):
-            df = pd.read_json(lista_directorio[j])
-            print("archivo desde directorio")
-            print(lista_directorio[j])
-            #pprint(lista_directorio[j])
-            #print(df.shape)
-            nombre_archivo = lista_directorio[j].split(".")[0]
-            print(nombre_archivo)
+        #print("Contenido subdirectorio: ", contenido_subdirectorio)
+        for j in range(len(contenido_subdirectorio)):
+            #print("Iteracion subdirectorio: ", j)
+            #print("Archivo: ", contenido_subdirectorio[j])
+            nombre_archivo_subdirectorio = ruta_bulk_s6_nombres +'/'+ contenido_ruta_bulk_s6[i] + '/'+ contenido_subdirectorio[j]
+            df = pd.read_json(nombre_archivo_subdirectorio)
+            pprint(df.shape)
             df = extraer_S6(df)
-            convert_files_parquet_h5(df, salida, nombre_archivo, x)
-            x+=1
-    if os.path.isfile('bulk-s6/'+ruta_bulk_s6[i]) == True:
-        #print("es un archivo")
-        nombre_archivo = 'bulk-s6/' + ruta_bulk_s6[i]
-        nombre_archivo.split(".")[0]
-        #pprint(nombre_archivo)
-        ##df = pd.read_json(nombre_archivo)
-        #pprint("nombre archivo desde raiz")
-        #pprint(df.shape)
-        #pprint(df.columns)
-        #convert_files_parquet(df, salida, nombre_archivo, i)
-    i+=1
-    print("iteracion" + str(i))
-    
+            convert_files_parquet_h5(df, salida_preprocesamiento_s6, contenido_subdirectorio[j], j)
+            #pprint(contenido_subdirectorio[j])
+            #df = pd.read_json(contenido_subdirectorio[j], lines=True)
+            #print("Tamaño del dataframe: ", df.shape)
