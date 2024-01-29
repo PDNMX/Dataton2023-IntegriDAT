a016d0940a23040d030220d5363b14bebec5f6b8
Héctor Franco <hhernandezf1800@alumno.ipn.mx>
2024-01-29 17:20:23 -0600

    script funcionando cruzando los datos del s6 con los del s3

--- scripts_PDN_Integridat/paso_3_procesar_cruzar_s6_s3.py
+++ scripts_PDN_Integridat/paso_3_procesar_cruzar_s6_s3.py
@@ -36,44 +36,34 @@ s6_df["parties_contactPoint_name"] = s6_df["parties_contactPoint_name"].str.norm
 df1 = s3_inhab.merge(s6_df, left_on="sancion_nombre", right_on="parties_name", how = "inner", suffixes = ("s3", "s6") )
 df2 = s3_inhab.merge(s6_df, left_on="sancion_nombre", right_on="parties_contactPoint_name", how = "inner", suffixes = ("s3", "s6") )
 df = pd.concat([df1, df2])
-pprint(df.shape)
-pprint(df.columns)
-pprint(df.head())
-print("Informacion del dataframe")
-pprint(df.info())
-##df = df.drop_duplicates(keep="last")
-##df = df.reset_index(drop = True)
+#df = df.apply(lambda x: str(x))
+df = df.drop_duplicates(["sancion_nombre","parties_name", "parties_contactPoint_name"],keep="last")
+#df = df.dropna(subset=["inhabilitacion_fechaInicial", "inhabilitacion_fechaFinal", "earliest_contractPeriod_startDate", "latest_contractPeriod_endDate"])
 
-##pprint(df.shape)
-##pprint(df.columns)
-##pprint(df.head())
-#pprint("identificando duplicados")
-#pprint(df.duplicated())
-
-#df = df.drop_duplicates(keep="last")
-#print(df.shape)
-#print(df.columns)
-#pprint(df.head())
-""" 
-df = df.dropna(subset=["inhabilitacion_fechaInicial", "inhabilitacion_fechaFinal", "earliest_contractPeriod_startDate", "latest_contractPeriod_endDate"])
+df.earliest_contractPeriod_startDate = pd.to_datetime(df.earliest_contractPeriod_startDate, utc=True)
+df.latest_contractPeriod_endDate = pd.to_datetime(df.latest_contractPeriod_endDate, utc=True)
+df.inhabilitacion_fechaInicial = pd.to_datetime(df.inhabilitacion_fechaInicial, utc=True)
+df.inhabilitacion_fechaFinal = pd.to_datetime(df.inhabilitacion_fechaFinal, utc=True)
 
 mask1 = df.earliest_contractPeriod_startDate.between(df.inhabilitacion_fechaInicial, df.inhabilitacion_fechaFinal)
+#pprint(mask1)
 mask2 = df.latest_contractPeriod_endDate.between(df.inhabilitacion_fechaInicial, df.inhabilitacion_fechaFinal)
+#pprint(mask2)
+
 final_mask = mask1 | mask2
 df["contrato_durante_inhabilitacion"] = final_mask
 df["contrato_durante_inhabilitacion"] = df["contrato_durante_inhabilitacion"].astype(int)
 df_result = df[final_mask]
-#df_result = df
-
 
 df_result[["inhabilitacion_fechaInicial", "inhabilitacion_fechaFinal", "earliest_contractPeriod_startDate", "latest_contractPeriod_endDate"]]
 
 columnas_orden = ["sancion_nombre",'tipo_persona', 'sancion_tipoPersona', 'sancion_objetoSocial', 'inhabilitacion_fechaInicial',
                   'inhabilitacion_fechaFinal','earliest_contractPeriod_startDate', 'latest_contractPeriod_endDate', 'parties_name', "parties_contactPoint_name",
-                  'ids3', 'expediente', '_id.$oid', 'ocid', 'ids6', 'contrato_durante_inhabilitacion']
+                  'ids3', 'expediente', '_id', 'ocid', 'ids6', 'contrato_durante_inhabilitacion']
 df_result = df_result[columnas_orden]
+
 df_result = df_result.reset_index(drop = True)
 
+df_result = df_result.astype(str)
 salida_paso3_resultado_cruzar_s3_s6 = "../salida_paso3_resultado_cruzar_s3_s6/"
 df_result.to_excel(salida_paso3_resultado_cruzar_s3_s6 + "contrato_durante_inhabilitacion_IntegriDAT.xlsx", index=False)
-"""
